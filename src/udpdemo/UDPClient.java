b2117543795921f581ee20cfaa1e1b9af070eb30
Sasha Klein-Charland <sp.kleincharland@gmail.com>
2026-04-02 16:37:36 -0400

    implement packet encoding and decoding across processes

--- src/udpdemo/UDPClient.java
+++ src/udpdemo/UDPClient.java
@@ -1,41 +1,75 @@
 package udpdemo;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
+import java.net.InetAddress;
+import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+
+import java.util.Random;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class UDPClient {
 
     public static void main(String[] args) throws Exception {
         // Create params hashmap 
-        String[] acceptedParams = {"srvr-ip", "srvr-port", "resource-name", "segment-size"};
-        Map<String, String> params = createParamsHashMap(args, acceptedParams);
+        // String[] acceptedParams = {"srvr-ip", "srvr-port", "resource-name", "segment-size"};
+        // Map<String, String> params = createParamsHashMap(args, acceptedParams);
+        // final InetAddress IP_ADDR = InetAddress.getLocalHost();
+        // final int SRVR_PORT_NUM = 9999;
 
-        final InetAddress IP_ADDR = InetAddress.getLocalHost();
-        final int SRVR_PORT_NUM = 9999;
+        final InetAddress SRVR_IP = InetAddress.getLocalHost();
+        final short SRVR_PORT = 9999;
+        final short MAX_SEG_SIZE = 512;
+        final String RESOURCE_NAME = "Paramore - This is Why.mp3";
+        byte seqNum = 0;
         
-        DatagramSocket ds = new DatagramSocket();
-        byte[] receiveBuf = new byte[1024];
+        DatagramSocket clientSocket = new DatagramSocket();
+        byte[] receiveBuffer = new byte[MAX_SEG_SIZE];
 
-        // Data
-        int i = 8;
-        byte[] b = String.valueOf(i).getBytes();
+        // BUILD PACKET
+        byte[] packetBody = RESOURCE_NAME.getBytes(StandardCharsets.UTF_8);
+        short payloadLength = (short) packetBody.length;
 
-        // Build packet wth IP_ADDR and PORT_NUM, and send
-        DatagramPacket sendPacket = new DatagramPacket(b, b.length, IP_ADDR, SRVR_PORT_NUM);
-        ds.send(sendPacket);
+        int connectionID = generateConnectionID();
 
-        // Receive any packets sent to this port
-        DatagramPacket receivePacket = new DatagramPacket(receiveBuf, receiveBuf.length);
-        ds.receive(receivePacket); 
+        byte[] sendPacketBytes = ByteBuffer.allocate(8 + payloadLength)
+            .putInt((int) connectionID)
+            .put((byte) seqNum)
+            .put(PacketType.REQUEST.code)
+            .putShort((short) payloadLength)
+            .put(packetBody, 0, payloadLength)
+            .array();
 
-        // Read data and print
-        String receiveData = new String(receivePacket.getData());
-        System.out.println("result: " + receiveData);
+        DatagramPacket sendPacket = new DatagramPacket(
+            sendPacketBytes, 
+            sendPacketBytes.length, 
+            SRVR_IP, 
+            SRVR_PORT
+        );
+        clientSocket.send(sendPacket);
 
-        ds.close();
+        System.out.println("[CLIENT] Sent packet.");
+        System.out.println("[CLIENT] Packet Info:");
+        System.out.println(" - Connection ID: " + connectionID);
+        System.out.println(" - Sequence Number: " + seqNum);
+        System.out.println(" - Type: " + PacketType.REQUEST);
+        System.out.println(" - Payload Length: " + payloadLength);
+        System.out.println("\nPacket Body:");
+        System.out.println(RESOURCE_NAME);
+
+
+        // DECODE RECEIVED PACKET
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        clientSocket.receive(receivePacket); 
+
+        byte[] receivePacketBytes = receivePacket.getData();
+
+
+        // System.out.println("result: " + receiveData);
+
+        clientSocket.close();
     }
 
     private static HashMap<String, String> createParamsHashMap(String[] args, String[] acceptedParams) throws IllegalArgumentException {
@@ -60,6 +94,11 @@ public class UDPClient {
 
         return returnMap;
 
+    }
+
+    private static int generateConnectionID() {
+        Random rand = new Random();
+        return (int) rand.nextInt(0, Integer.MAX_VALUE);
     }
 
 }
--- src/udpdemo/UDPServer.java
+++ src/udpdemo/UDPServer.java
@@ -3,51 +3,71 @@ package udpdemo;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class UDPServer {
 
     public static void main(String[] args) throws Exception {
 
-        final InetAddress IP_ADDR = InetAddress.getLocalHost();
-        final int SRVR_PORT_NUM = 9999;
+        final InetAddress SRVR_IP = InetAddress.getLocalHost();
+        final short SRVR_PORT = 9999;
+        final short MAX_SEG_SIZE = 512;
+        byte seqNum = 0;
         
-        // Create a socket for this server with SRVR_PORT_NUM
-        DatagramSocket ds = new DatagramSocket(SRVR_PORT_NUM);
-        byte[] receiveBuf = new byte[1024];
+        DatagramSocket serverSocket = new DatagramSocket(SRVR_PORT);
+        byte[] receiveBuffer = new byte[MAX_SEG_SIZE];
 
         System.out.println("[SERVER] Awaiting packets...");
 
         // Receive any packet addressed to this port
         DatagramPacket receivePacket = new DatagramPacket(
-            receiveBuf, 
-            receiveBuf.length
+            receiveBuffer,
+            receiveBuffer.length
         );
-        ds.receive(receivePacket);
+        serverSocket.receive(receivePacket);
 
         System.out.println("[SERVER] Received packet.");
         
-        // Parse packet data and compute result
-        String receiveData = new String(receivePacket.getData());
-        int num = Integer.parseInt(receiveData.trim());
-        int result = num*num;
-
-        System.out.println("[SERVER] Received data: " + num);
-        System.out.println("[SERVER] Result: " + result);
-
-        byte[] sendBuf = String.valueOf(result).getBytes();
-
-        // Build packet with IP_ADDR and port number of client, and send
-        DatagramPacket sendPacket = new DatagramPacket(
-            sendBuf, 
-            sendBuf.length, 
-            IP_ADDR, 
-            receivePacket.getPort()
+        // Decode packet data
+        byte[] receivePacketBytes= receivePacket.getData();
+        
+        int receivedConnectionID = ByteBuffer.wrap(
+            Arrays.copyOfRange(receivePacketBytes, 0, 4)
+        ).getInt();
+        byte receivedSequenceNumber = receivePacketBytes[4];
+        PacketType receivedType = PacketType.fromCode(
+            receivePacketBytes[5]
         );
-        ds.send(sendPacket);
+        short receivedPayloadLength = ByteBuffer.wrap(
+            Arrays.copyOfRange(receivePacketBytes, 6, 8)
+        ).getShort();
+        byte[] receivedPayload = Arrays.copyOfRange(receivePacketBytes, 8, receivePacketBytes.length);
 
-        System.out.println("[SERVER] Sending result to client.");
+        System.out.println("Packet Info:");
+        System.out.println(" - Connection ID: " + receivedConnectionID);
+        System.out.println(" - Sequence Number: " + receivedSequenceNumber);
+        System.out.println(" - Type: " + receivedType);
+        System.out.println(" - Payload Length: " + receivedPayloadLength);
+        System.out.println("\nDecoded Packet:");
+        System.out.println(new String(receivedPayload, StandardCharsets.UTF_8));
 
-        ds.close();
+
+        // byte[] sendBuf = String.valueOf(result).getBytes();
+
+        // // Build packet with IP_ADDR and port number of client, and send
+        // DatagramPacket sendPacket = new DatagramPacket(
+        //     sendBuf, 
+        //     sendBuf.length, 
+        //     IP_ADDR, 
+        //     receivePacket.getPort()
+        // );
+        // ds.send(sendPacket);
+
+        // System.out.println("[SERVER] Sending result to client.");
+
+        serverSocket.close();
     }
     
 }
