93e2a0e2672ec30f06a6113546f1951031ff45ce
Sasha <sp.kleincharland@gmail.com>
2026-04-05 15:10:07 -0400

    accept cli parameters

--- src/udpdemo/UDPClient.java
+++ src/udpdemo/UDPClient.java
@@ -7,57 +7,45 @@ import java.net.DatagramSocket;
 public class UDPClient {
 
     public static void main(String[] args) throws Exception {
-        // Create params hashmap 
-        // String[] acceptedParams = {"srvr-ip", "srvr-port", "resource-name", "segment-size"};
-        // Map<String, String> params = createParamsHashMap(args, acceptedParams);
-        // final InetAddress IP_ADDR = InetAddress.getLocalHost();
-        // final int SRVR_PORT_NUM = 9999;
+        // Default values
+        InetAddress serverAddress = InetAddress.getByName("localhost");
+        short serverPort = 9999;
+        String resourceName = "ulysses.txt";
+        short maxSegmentSize = 512;
 
-        // Set by params
-        final InetAddress SRVR_IP = InetAddress.getLocalHost();
-        final short SRVR_PORT = 9999;
-        final short MAX_SEG_SIZE = 512;
-        final String RESOURCE_NAME = "crime-and-punishment.txt";
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].equals("--address")) {
+                serverAddress = InetAddress.getByName(args[i+1]);
+            }
+
+            if (args[i].equals("--port")) {
+                serverPort = Short.parseShort(args[i+1]);
+            }
+
+            if (args[i].equals("--resource-name")) {
+                resourceName = args[i+1];
+            }
+
+            if (args[i].equals("--segment-size")) {
+                maxSegmentSize = Short.parseShort(args[i+1]);
+            } 
+        }
 
         DatagramSocket clientSocket = new DatagramSocket();
         
         ClientSession session = new ClientSession(
             clientSocket, 
-            SRVR_IP, 
-            SRVR_PORT, 
-            RESOURCE_NAME, 
-            MAX_SEG_SIZE
+            serverAddress, 
+            serverPort, 
+            resourceName, 
+            maxSegmentSize
         );
 
-        System.out.println("Created a session for " + RESOURCE_NAME);
+        System.out.println("Created a session for " + resourceName);
         session.run();
-        System.out.println("Successfully received " + RESOURCE_NAME);
+        System.out.println("Successfully received " + resourceName);
         
         clientSocket.close();
-    }
-
-    private static HashMap<String, String> createParamsHashMap(String[] args, String[] acceptedParams) throws IllegalArgumentException {
-        if (args.length % 2 != 0) throw new IllegalArgumentException("All parameters must be passed with the format \'--parameter-name value\'.");
-
-        HashMap<String, String> returnMap = new HashMap<>();
-
-        for (int i = 0; i < args.length; i += 2) {
-            if (!args[i].startsWith("--")) {
-                continue;
-            }
-
-            // Remove leading "--"
-            String paramName = args[i].substring(2);
-            for (String a : acceptedParams) {
-                if (a.equals(paramName)) {
-                    String paramValue = args[i+1];
-                    returnMap.put(paramName, paramValue);
-                }
-            }
-        }
-
-        return returnMap;
-
     }
 
 }
